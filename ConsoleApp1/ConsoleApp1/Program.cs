a2ada54ac4d3dd5173cda7281c4ce09790786ae7
Nikolayski <74212719+Nikolayski@users.noreply.github.com>
2021-05-18 17:07:41 +0300

    Update code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,7 +14,6 @@ namespace ConsoleApp1
 
         }
 
-
         private static void BubbleSort(int[] arr)
         {
             for (int i = 0; i < arr.Length - 1; i++)
@@ -76,7 +75,7 @@ namespace ConsoleApp1
 
         private static int Partition(int[] arr, int start, int end)
         {
-            int pivot = arr[end];   
+            int pivot = arr[end];
             int i = start;
             int temp;
             for (int j = start; j < end; j++)
@@ -96,62 +95,20 @@ namespace ConsoleApp1
             return i;
         }
 
-        private static int[] MergeSort(int[] arr)
+        private static void MergeSort(int[] input, int left, int right)
         {
-            if (arr.Length <= 1)
+            if (left < right)
             {
-                return arr;
+                int middle = (left + right) / 2;
+
+                MergeSort2(input, left, middle);
+                MergeSort2(input, middle + 1, right);
+
+                Merge2(input, left, middle, right);
             }
-
-            int middle = arr.Length / 2;
-            int[] left = new int[arr.Length / 2];
-            int[] right = arr.Length % 2 == 0 ? new int[arr.Length / 2] : new int[arr.Length / 2 + 1];
-
-            Array.Copy(arr, 0, left, 0, middle);
-            Array.Copy(arr, middle, right, 0, arr.Length - middle);
-
-            left = MergeSort(left);
-            right = MergeSort(right);
-            var result = Merge(left.ToList(), right.ToList());
-            return result.ToArray();
         }
 
-        private static List<int> Merge(List<int> left, List<int> right)
-        {
-            List<int> result = new List<int>();
-
-            while (left.Count > 0 || right.Count > 0)
-            {
-                if (left.Count > 0 && right.Count > 0)
-                {
-                    if (left.First() <= right.First())
-                    {
-                        result.Add(left.First());
-                        left.RemoveAt(0);
-                    }
-                    else
-                    {
-                        result.Add(right.First());
-                        right.RemoveAt(0);
-                    }
-                }
-                else if (left.Count > 0)
-                {
-                    result.Add(left.First());
-                    left.RemoveAt(0);
-                }
-                else if (right.Count > 0)
-                {
-                    result.Add(right.First());
-                    right.RemoveAt(0);
-                }
-            }
-
-            return result;
-        }
-
-        // MERGE SORT 2
-        private static void Merge2(int[] input, int left, int middle, int right)
+        private static void Merge(int[] input, int left, int middle, int right)
         {
             int[] leftArray = new int[middle - left + 1];
             int[] rightArray = new int[right - middle];
@@ -186,19 +143,7 @@ namespace ConsoleApp1
             }
         }
 
-        private static void MergeSort2(int[] input, int left, int right)
-        {
-            if (left < right)
-            {
-                int middle = (left + right) / 2;
-
-                MergeSort2(input, left, middle);
-                MergeSort2(input, middle + 1, right);
-
-                Merge2(input, left, middle, right);
-            }
-        }
-        private static void CountSort(int[] arr, int maxNumber)    // only counting repeating numbers, but the arr is not sorted at the end ...  Could be modified.
+        private static void CountSort(int[] arr, int maxNumber)
         {
             var result = new int[maxNumber + 1];
 
@@ -223,7 +168,7 @@ namespace ConsoleApp1
         private static int BinarySearchLinear(int[] arr, int searchKey)
         {
             var min = 0;
-            var max = arr.Length - 1; 
+            var max = arr.Length - 1;
 
             while (min <= max)
             {
